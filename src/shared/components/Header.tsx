fc9f8d81b6b1a503841a79f94d9d4ad05c977102
FloraHmmnd <89636863+FloraHmmnd@users.noreply.github.com>
2023-02-17 14:34:35 +0100

    Hssh 77 add logo (#5)
    * ✨ add logo
    
    * ✨ add logo
    
    * ✨ improve margin in join page
    
    * logo above log box
    
    ---------
    
    Co-authored-by: FloraHmmnd <flora.hommand@jellysmack.com>
    Co-authored-by: Aurelie D <adelaunay@palo-it.com>

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom';
 import ArtistConnectButtons from './ArtistConnectButtons';
 import Box from './Box';
 import MetamaskConnectionButton from './MetamaskConnectionButton';
+import { ReactComponent as Logo } from '../../assets/svg/logo.svg';
 
 const Header: React.FC = () => {
   return (
     <header className="w-screen">
+      <Link to={'../home'}>
+        <Logo title="HouseShow logo" className="w-96 pl-4 h-28"></Logo>
+      </Link>
       <Box
         childComponent={
           <div className="flex items-center justify-between">
             <ArtistConnectButtons />
-            <Link to={'../home'}>HOUSE/SHOW</Link>
             <MetamaskConnectionButton />
           </div>
         }
