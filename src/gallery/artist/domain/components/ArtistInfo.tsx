437d6ccbe999b1e7ac353709f3a7d6ead0ce9e78
Aurelie D <adelaunay@palo-it.com>
2023-03-02 15:32:20 +0100

    copy of correct code to avoid erasures depending of PRs order

--- src/gallery/artist/domain/components/ArtistInfo.tsx
+++ src/gallery/artist/domain/components/ArtistInfo.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import useFetchArtistDetails from '../hooks/fetchArtistDetails.hook';
 import ArtistInfoLeft from './ArtistInfoLeft';
 import ArtistInfoRight from './ArtistInfoRight';
 
 const ArtistInfo: React.FC = () => {
+  const { id } = useParams();
   const { artist, isArtistLoading, error } = useFetchArtistDetails(
-    // TODO add a valid id please
-    '63fe10e7f2e3c164b09e831d'
+    id as string
   );
   if (isArtistLoading) {
     return <h2 className="m-4">Loading...</h2>;
