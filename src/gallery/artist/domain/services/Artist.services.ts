4d799e540ae18c59aa50104f0b629ce2367b79d4
Aurelie D <adelaunay@palo-it.com>
2023-03-02 21:39:59 +0100

    env with localhost URL and token

--- src/gallery/artist/domain/services/Artist.services.ts
+++ src/gallery/artist/domain/services/Artist.services.ts
@@ -1,12 +1,17 @@
 import { IArtistForm, IAddNftForm } from './../types/artist.type';
 import type { IArtistResponse, IArtistsResponse } from '../types/artist.type';
 
-const URL = 'http://localhost:8000/';
+const AUTH_URL = import.meta.env.VITE_AUTH_URL;
+
+export const fetchAllArtistsInfos = async (): Promise<IArtistsResponse> => {
+  const response = await fetch(`${AUTH_URL}/`);
+  return response.json();
+};
 
 export const fetchArtistDetails = async (
   id: string
 ): Promise<IArtistResponse> => {
-  const response = await fetch(`${URL}artists/${id}`);
+  const response = await fetch(`${AUTH_URL}/${id}`);
   return response.json();
 };
 
@@ -14,30 +19,23 @@ export const updateArtistProfile = async (
   id: string,
   params: IArtistForm | IAddNftForm
 ) => {
-  const response = await fetch(`${URL}artists/${id}`, {
+  const response = await fetch(`${AUTH_URL}/${id}`, {
     method: 'PATCH',
     body: JSON.stringify(params),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
-      Authorization: 'RANDOM_TOKEN_SECRET',
+      Authorization: import.meta.env.VITE_AUTH_SECRET,
     },
   });
   return response.json();
 };
 
-export const fetchAllArtistsInfos = async (): Promise<IArtistsResponse> => {
-  const response = await fetch(`${URL}artists/`);
-  return response.json();
-};
-
 export const deleteArtist = async (id: string) => {
-  const URL = 'http://localhost:8000/artists';
-
-  const response = await fetch(`${URL}/${id}`, {
+  const response = await fetch(`${AUTH_URL}/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
-      Authorization: 'RANDOM_TOKEN_SECRET',
+      Authorization: import.meta.env.VITE_AUTH_SECRET,
     },
   });
   if (!response) {
